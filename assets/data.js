8a253f742c5f0df93a021af7a79499d086fff82f
EsratJahanJui <esratjahanjui327@gmail.com>
2024-11-05 19:04:21 +0600

    personal portfolio

---
+++ assets/data.js
@@ -0,0 +1,30 @@
+const servicesData = [
+  {
+    title: "Frontend Devlopment",
+    category: "frontend",
+    description: [
+       "I offer comprehensive website design and development services, creating visually appealing, responsive, and user-friendly websites tailored to your needs. Utilizing HTML, CSS, JavaScript, React, and Tailwind CSS,I bring your vision to life with modern web standards. transforming your designs into interactive, high-performance web applications. With expertise in React and Tailwind CSS,  I build efficient, maintainable, and scalable solutions that meet your business goals.
+       "
+    ],
+  },
+
+  // {
+  //   title: "Backend Development",
+  //   category: "backend",
+  //   description: [
+  //     "Backend Lorem ipsum dolor sit. amet consectetur adipisicing elit Id expedita officiis Odio pariatur optio reprehenderit Fuga,quisquam quod. Ea saepe dolorem quia delectus vel similique at sit corrupti repudiandae excepturi.",
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, nemo earum veritatis obcaecati dolore dolor ipsam sunt accusamus tenetur laboriosam aliquid blanditiis ut odit atque eos eaque doloremque quia sint?",
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, nemo earum veritatis obcaecati dolore dolor ipsam sunt accusamus tenetur laboriosam aliquid blanditiis ut odit atque eos eaque doloremque quia sint?",
+  //   ],
+  // },
+
+  // {
+  //   title: "Fullstack Development",
+  //   category: "fullstack",
+  //   description: [
+  //     "Backend Lorem ipsum dolor sit. amet consectetur adipisicing elit Id expedita officiis Odio pariatur optio reprehenderit Fuga,quisquam quod. Ea saepe dolorem quia delectus vel similique at sit corrupti repudiandae excepturi.",
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, nemo earum veritatis obcaecati dolore dolor ipsam sunt accusamus tenetur laboriosam aliquid blanditiis ut odit atque eos eaque doloremque quia sint?",
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, nemo earum veritatis obcaecati dolore dolor ipsam sunt accusamus tenetur laboriosam aliquid blanditiis ut odit atque eos eaque doloremque quia sint?",
+  //   ],
+  // },
+];
